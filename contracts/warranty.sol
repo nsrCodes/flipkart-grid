75a47308eeb619574d8912afccee0f41142949b1
Rishabh1410 <60822949+Rishabh1410@users.noreply.github.com>
2022-07-30 15:22:53 +0530

    Update warranty.sol
    payble mint

--- contracts/warranty.sol
+++ contracts/warranty.sol
@@ -1,33 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
 
 contract warrantyToken is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-
+    mapping(string => uint8) existingURIs;
+    
     event Attest(address indexed to, uint256 indexed tokenId);
     event Revoke(address indexed to, uint256 indexed tokenId);
 
     constructor() ERC721("warrantyToken", "WTK") {}
 
-    function safeMint(address to, string memory uri) public payable {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+        existingURIs[uri] = 1;
+    }
+    function payToMint(address recipient, string memory metadataURI)
+        public
+        payable
+        returns (uint256)
+    {
+        require(existingURIs[metadataURI] != 1, "NFT already minted!");
+        require(msg.value >= 0.00 ether, "Need to pay up!");
+
+        uint256 newItemId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        existingURIs[metadataURI] = 1;
+
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, metadataURI);
+
+        return newItemId;
     }
 
     function burn(uint256 tokenId) external {
-        require(
-            ownerOf(tokenId) == msg.sender,
-            "Only owner of the token can burn it"
-        );
+        require(ownerOf(tokenId) == msg.sender, "Only owner of the token can burn it");
         _burn(tokenId);
     }
 
@@ -35,22 +51,12 @@ contract warrantyToken is ERC721, ERC721URIStorage, Ownable {
         _burn(tokenId);
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256
-    ) internal pure override {
-        require(
-            from == address(0) || to == address(0),
-            "Not allowed to transfer token"
-        );
+    function _beforeTokenTransfer(address from, address to, uint256) pure override internal {
+        require(from == address(0) || to == address(0), "Not allowed to transfer token");
     }
 
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override {
+    function _afterTokenTransfer(address from, address to, uint256 tokenId) override internal {
+
         if (from == address(0)) {
             emit Attest(to, tokenId);
         } else if (to == address(0)) {
@@ -58,10 +64,7 @@ contract warrantyToken is ERC721, ERC721URIStorage, Ownable {
         }
     }
 
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
